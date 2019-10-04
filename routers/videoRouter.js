4f2ced2d25a09dc3591d45e85c839d9e0b33a24c
obidkor <coavldjs93@naver.com>
2019-10-04 18:30:17 +0900

    before eslint

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,16 +1,23 @@
 import express from "express";
 import routes from "../routes";
-import { getUpload,postUpload, videoDetail, editVideo, deleteVideo } from "../controller/videoController";
+import { getUpload,postUpload, videoDetail, getEditVideo,postEditVideo, deleteVideo } from "../controller/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
-// videoRouter.get(routes.videos,videos);
+//upload
 videoRouter.get(routes.upload,getUpload);
 videoRouter.post(routes.upload,uploadVideo,postUpload);
+
+//video detail
 videoRouter.get(routes.videoDetail(),videoDetail);
-videoRouter.get(routes.editVideo,editVideo);
-videoRouter.get(routes.deleteVideo,deleteVideo);
+
+//edit video
+videoRouter.get(routes.editVideo(),getEditVideo);
+videoRouter.post(routes.editVideo(),postEditVideo);
+
+//deletevideo
+videoRouter.get(routes.deleteVideo(),deleteVideo);
 
 
 export default videoRouter;
