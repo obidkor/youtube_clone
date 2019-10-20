0710125869f021da67e4bc688dbb066a3734d1fa
obidkor <coavldjs93@naver.com>
2019-10-20 19:31:21 +0900

    heroku commit8

---
+++ build/db.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var _mongoose = _interopRequireDefault(require("mongoose"));
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_dotenv["default"].config();
+
+_mongoose["default"].connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+var db = _mongoose["default"].connection;
+
+var handleOpen = function handleOpen() {
+  console.log("✅ connected to db");
+};
+
+var handleError = function handleError(error) {
+  console.log("\u274C Error on DB connection : ".concat(error));
+};
+
+db.once("open", handleOpen);
+db.on("error", handleError);
---
+++ build/passport.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var _passport = _interopRequireDefault(require("passport"));
+
+var _passportGithub = _interopRequireDefault(require("passport-github"));
+
+var _passportFacebook = _interopRequireDefault(require("passport-facebook"));
+
+var _User = _interopRequireDefault(require("./models/User"));
+
+var _userController = require("./controller/userController");
+
+var _routes = _interopRequireDefault(require("./routes"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_passport["default"].use(_User["default"].createStrategy());
+
+_passport["default"].use(new _passportGithub["default"]({
+  clientID: process.env.GH_ID,
+  clientSecret: process.env.GH_SECRET,
+  callbackURL: "http://localhost:4000".concat(_routes["default"].githubCallback)
+}, _userController.githubLoginCallback));
+
+_passport["default"].use(new _passportFacebook["default"]({
+  clientID: process.env.FB_ID,
+  clientSecret: process.env.FB_SECRET,
+  callbackURL: "https://clever-snail-21.localtunnel.me".concat(_routes["default"].facebookCallback),
+  profileFields: ["id", "displayName", "photos", "email"],
+  scope: ["public_profile", "email"]
+}, _userController.facebookLoginCallback)); // github 인증에서 email이 null로 저장될 경우에 쓸 것
+// 근데 이걸 쓰면 req.user에 사용자 쿠키가 저장이 안됨 ㅠㅠ
+// passport.serializeUser((user, done) => done(null, user));
+// passport.deserializeUser((user, done) => done(null, user));
+
+
+_passport["default"].serializeUser(_User["default"].serializeUser());
+
+_passport["default"].deserializeUser(_User["default"].deserializeUser());
